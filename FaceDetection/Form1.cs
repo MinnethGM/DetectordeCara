186b6d0847ba641c24232253bf965340b726e8b7
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-11-01 10:40:14 -0700

    Agregar archivos de proyecto.

---
+++ FaceDetection/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace FaceDetection
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> imgInput;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    imgInput = new Image<Bgr, byte>(dialog.FileName);
+                    pictureBox1.Image = imgInput.Bitmap;
+                }
+                else
+                {
+                    throw new Exception("Selecciona una imagen");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+                                                                                                                                        
+        }
+
+        private void detectFaceHarrToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (imgInput == null)
+                {
+                    throw new Exception("Selecciona una imagen");
+                }
+                DetectFaceHaar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    public void DetectFaceHaar()
+        {
+            try
+            {
+                string facePath = Path.GetFullPath(@"../../data/haarcascade_frontalface_default.xml");
+
+                CascadeClassifier classifierFace = new CascadeClassifier(facePath);
+
+                var imgGray = imgInput.Convert<Gray, byte>().Clone();
+                Rectangle[] faces = classifierFace.DetectMultiScale(imgGray, 1.1, 4);
+                foreach (var face in faces)
+                {
+                    imgInput.Draw(face, new Bgr(0, 0, 255), 2);
+                }
+                pictureBox1.Image = imgInput.Bitmap;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
